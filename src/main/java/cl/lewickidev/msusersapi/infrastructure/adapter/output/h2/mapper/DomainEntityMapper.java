bb20c6ef8fdc438d07613f56b57be11a24d04493
Michael Lewicki <lewickiclass@gmail.com>
2024-05-24 06:01:10 -0400

    DomainEntityMapper was fixed

--- src/main/java/cl/lewickidev/msusersapi/infrastructure/adapter/output/h2/mapper/DomainEntityMapper.java
+++ src/main/java/cl/lewickidev/msusersapi/infrastructure/adapter/output/h2/mapper/DomainEntityMapper.java
@@ -5,6 +5,7 @@ import cl.lewickidev.msusersapi.domain.model.User;
 import cl.lewickidev.msusersapi.infrastructure.adapter.output.h2.entity.PhoneEntity;
 import cl.lewickidev.msusersapi.infrastructure.adapter.output.h2.entity.UserEntity;
 import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
 import org.mapstruct.MappingConstants.ComponentModel;
 import org.mapstruct.NullValueCheckStrategy;
 import org.springframework.data.domain.Page;
@@ -22,6 +23,7 @@ public interface DomainEntityMapper {
 
     Phone toDTO(PhoneEntity entity);
 
+    @Mapping(target = "user", ignore = true)
     PhoneEntity toEntity(Phone dto);
 
     default Page<Phone> toPhoneDTOs(Page<PhoneEntity> phoneEntities) {
